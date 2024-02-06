1eb03061fdff87bcb0b7a82fd1aba4482849cc0a
mathieu-tatat <70898925+crosslink87@users.noreply.github.com>
2024-02-06 09:42:45 +0100

    first commit

---
+++ src/main/java/com/openclassrooms/OpenclassroomsApplication.java
@@ -0,0 +1,13 @@
+package com.openclassrooms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpenclassroomsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OpenclassroomsApplication.class, args);
+	}
+
+}
